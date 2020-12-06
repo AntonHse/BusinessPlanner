4be9ac8ad958ef7cefc5fe9c6a86b0d0e6e1da91
Шуплецов Антон <shupletsov.anton@gmail.com>
2020-12-06 14:01:51 +0300

    Initial Commit

---
+++ BusinessPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusinessPlanner
+//
+//  Created by Антон Шуплецов on 06.12.2020.
+//  Copyright © 2020 Anton Shupletsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
